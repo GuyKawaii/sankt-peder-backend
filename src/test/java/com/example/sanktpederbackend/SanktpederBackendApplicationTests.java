7d53b80590cb7f6f11b4f2d1453c2903b3fd977f
Malikktk <mali0237@stud.kea.dk>
2023-05-01 12:51:24 +0200

    Start

---
+++ src/test/java/com/example/sanktpederbackend/SanktpederBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanktpederbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanktpederBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
